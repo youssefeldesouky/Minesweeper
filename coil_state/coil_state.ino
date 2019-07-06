5e6144348e712574f6a28b9076305394bf8217cf
youssef <zoidberg2010eaf@gmail.com>
2019-07-06 20:36:03 +0200

    Added coil_state arduino sketch

---
+++ coil_state/coil_state.ino
@@ -0,0 +1,30 @@
+#include <ros.h>
+#include <std_msgs/Int8.h>
+
+const uint8_t IND = 4, IR = 5;
+std_msgs::Int8 STATE;
+ros::NodeHandle nh;
+uint8_t _state;
+ros::Publisher coil_pub("coil_state", &STATE);
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(IND, INPUT);
+  pinMode(IR, INPUT);
+  nh.initNode();
+  nh.advertise(coil_pub);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(digitalRead(IND) && !digitalRead(IR)){
+    _state = 1;
+  }else if(digitalRead(IND) && digitalRead(IR)){
+    _state = 2;
+  }else if(!digitalRead(IND) && !digitalRead(IR)){
+    _state = 0;
+  }
+  STATE.data = _state;
+  coil_pub.publish(&STATE);
+  nh.spinOnce();
+  delay(20);
+}
