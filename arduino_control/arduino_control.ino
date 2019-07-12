ad40d6156416fde082b32ec03c5698024de4db53
youssef <joesamir2010eaf@gmail.com>
2019-07-12 14:40:48 +0200

    Added the arduino_control sketch
    Added the sketch that'll be uploaded to the controlling arduino which combines the drivers controlling and joint states listener

---
+++ arduino_control/arduino_control.ino
@@ -0,0 +1,153 @@
+//Arduino Uno is a total piece of shit
+#include <ros.h>
+#include <std_msgs/Int8MultiArray.h>
+#include <std_msgs/Float32MultiArray.h>
+#include <std_msgs/Float32.h>
+#include <std_msgs/Int16.h>
+#define L_EN_F 4
+#define R_EN_F 5 
+#define L_EN_B 6
+#define R_EN_B 7
+#define L_PWM_F 8
+#define L_PWM_B 9
+#define R_PWM_B 10
+#define R_PWM_F 11
+uint8_t L_PWM = 0 , R_PWM = 0 ;
+ros::NodeHandle nh;
+uint8_t l_pwm=0,r_pwm=0;
+const uint8_t L = 2, R = 3;
+
+int32_t l_ticks, r_ticks;
+const uint16_t maxTicks = 970;  //PPR
+double l_pos, r_pos;
+int8_t l_sign = 1, r_sign = 1;
+int16_t left_cmd, right_cmd;
+
+void signCB(const std_msgs::Int8MultiArray &data){
+  l_sign = data.data[0];
+  r_sign = data.data[1];
+}
+
+void leftCmdCB(const std_msgs::Int16 &data){
+  left_cmd = (int16_t)data.data;
+}
+
+void rightCmdCB(const std_msgs::Int16 &data){
+  right_cmd = (int16_t)data.data;
+}
+
+ros::Subscriber<std_msgs::Int8MultiArray> sign_sub("sign", &signCB);
+ros::Subscriber<std_msgs::Int16> left_cmd_sub("left_cmd", &leftCmdCB);
+ros::Subscriber<std_msgs::Int16> right_cmd_sub("right_cmd", &rightCmdCB);
+std_msgs::Float32MultiArray joint_states;
+std_msgs::Int16 right_msg;
+std_msgs::Int16 left_msg;
+
+ros::Publisher js_pub("joint_positions", &joint_states);
+void setup() {
+  pinMode(4,OUTPUT);
+    pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(10,OUTPUT);
+  pinMode(11,OUTPUT);
+  pinMode(12,OUTPUT);
+
+  pinMode(L, INPUT_PULLUP);
+  pinMode(R, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(L), leftENC, RISING);
+  attachInterrupt(digitalPinToInterrupt(R), rightENC, RISING);
+  nh.initNode();
+  nh.subscribe(sign_sub);
+  nh.subscribe(left_cmd_sub);
+  nh.subscribe(right_cmd_sub);
+  joint_states.data_length = 4;
+  nh.advertise(js_pub);
+digitalWrite(R_EN_F,LOW);
+digitalWrite(R_EN_B,LOW);
+digitalWrite(L_EN_F,LOW);
+digitalWrite(L_EN_B,LOW);
+}
+
+void loop() {
+  Motor_Drive();
+  l_pos = ((double)l_ticks/980) * 2 * PI;
+  r_pos = ((double)r_ticks/980) * 2 * PI;
+  float positions[4] = {l_pos, r_pos, l_pos, r_pos};
+  joint_states.data = positions;
+  js_pub.publish(&joint_states);
+  nh.spinOnce();
+  delay(20);
+}
+
+void leftENC(){
+  l_ticks += l_sign;
+}
+
+void rightENC(){
+  r_ticks += r_sign;
+}
+//Receive data from 2 encoders (2 Interrupts)
+//Control 4 Motors with 2 control signals (one for each side)
+//tick-meters could overflow
+void Motor_Drive (void)
+{
+  digitalWrite(L_EN_F,LOW);
+  digitalWrite(R_EN_F,LOW);
+  digitalWrite(L_EN_B,LOW);
+  digitalWrite(R_EN_B,LOW);
+  analogWrite(L_PWM_B,0);
+  analogWrite(R_PWM_B,0);
+  analogWrite(L_PWM_F,0);
+  analogWrite(R_PWM_F,0);
+  //delay(30);
+  l_pwm= map(abs(left_cmd), 4, 183, 80, 255);
+  r_pwm = map(abs(right_cmd), 4, 183, 80, 255);
+  
+  if (right_cmd < 0 ) // right side reverse direction 
+  {
+   digitalWrite(R_EN_F,HIGH);  
+   digitalWrite(R_EN_B,HIGH);
+   //from RPM to PWM where x is the pwm vaule
+   analogWrite(R_PWM_B,abs(r_pwm));
+    }
+  else if (right_cmd > 0) //right side forward
+  {
+    
+   digitalWrite(R_EN_B,HIGH);  
+   digitalWrite(R_EN_F,HIGH);  
+   //from RPM to PWM where x is the pwm vaule
+   analogWrite(R_PWM_F,abs(r_pwm));   
+    }
+      else 
+    {
+    r_pwm = l_pwm = 0;
+    digitalWrite(R_EN_B,LOW);  
+    digitalWrite(R_EN_F,LOW);     
+      }
+  
+  if (left_cmd  < 0 ) // left side reverse direction 
+  {
+   digitalWrite(L_EN_F,HIGH);  
+   digitalWrite(L_EN_B,HIGH);
+   //from RPM to PWM where x is the pwm vaule
+   analogWrite(L_PWM_B,abs(l_pwm));
+    }
+  else if (left_cmd  > 0) //left side forward
+  {
+   digitalWrite(L_EN_B,HIGH);  
+   digitalWrite(L_EN_F,HIGH);  
+   //from RPM to PWM where x is the pwm vaule
+   
+   analogWrite(L_PWM_F,abs(l_pwm));   
+    }
+    else 
+    {
+     l_pwm = r_pwm = 0;
+    digitalWrite(L_EN_B,LOW);  
+    digitalWrite(L_EN_F,LOW); 
+      }
+}
+  
